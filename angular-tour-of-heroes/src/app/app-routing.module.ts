776654c45435533220014f89b5e1af96bc6d641f
holliee <hobrien@seas.upenn.edu>
2022-06-02 13:37:37 -0700

    Adding a dashboard view

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //path: A string that matches the URL in the browser address bar.
+  //component: The component that the router should create when navigating to this route.
+
+  { path: 'heroes', component: HeroesComponent },
+];
 
 @NgModule({
+  //The following line adds the RouterModule to the AppRoutingModule imports array and configures it with the routes in one step by calling RouterModule.forRoot():
+
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
