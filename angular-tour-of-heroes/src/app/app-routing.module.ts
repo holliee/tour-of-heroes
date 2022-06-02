b5d89087441c2f7e329f0c63dabbda06a33c6094
holliee <hobrien@seas.upenn.edu>
2022-06-02 13:59:39 -0700

    added default and dashboard route

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   //path: A string that matches the URL in the browser address bar.
   //component: The component that the router should create when navigating to this route.
 
   { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
